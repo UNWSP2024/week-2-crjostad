cec3a2079d87ceda54652831e015ec2859d3e764
crjostad <crjostad@students.unwsp.edu>
2024-09-13 11:06:09 -0500

    Update program_4.py

--- program_4.py
+++ program_4.py
@@ -1,4 +1,4 @@
 # Enter degrees in Celsius after degrees_celsius
-degrees_celsius = (input)
+degrees_celsius = (input(5))
 temp_conversion = (float(9/5*degrees_celsius)) + 32
 print(temp_conversion, "degrees fahrenhiet")
