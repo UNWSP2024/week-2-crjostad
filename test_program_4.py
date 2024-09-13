26c7fa43050f1fae0701e735803ca7c1644810c6
crjostad <crjostad@students.unwsp.edu>
2024-09-13 11:24:21 -0500

    Update test_program_4.py

--- test_program_4.py
+++ test_program_4.py
@@ -1,12 +1,1 @@
-# Function to convert Celsius to Fahrenheit
-def celsius_to_fahrenheit(celsius):
-    return (9/5) * celsius + 32
 
-# Ask the user to enter a temperature in Celsius
-celsius = float(input("Enter temperature in Celsius: "))
-
-# Convert the temperature to Fahrenheit
-fahrenheit = celsius_to_fahrenheit(celsius)
-
-# Display the temperature in Fahrenheit
-print(f"{celsius}°C is equal to {fahrenheit}°F")
