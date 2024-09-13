0dc9a99ea15a481c26093ee046e9cd5c114bbe65
crjostad <crjostad@students.unwsp.edu>
2024-09-13 10:55:14 -0500

    Update program_4.py

--- program_4.py
+++ program_4.py
@@ -1,4 +1,4 @@
 # Enter degrees in Celsius after degrees_celsius
 degrees_celsius = 44
 degrees_fahrenhiet = (9/5*degrees_celsius) + 32
-print(degrees_fahrenhiet)
+print(degrees_fahrenhiet, "degrees fahrenhiet")
