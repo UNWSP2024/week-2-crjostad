ec79d16294de18929b5515f2bde359c8fd315bfa
crjostad <crjostad@students.unwsp.edu>
2024-09-13 08:21:37 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,4 +1,4 @@
-  print("Caleb Jostad")
-print("2857 70th st, Granada MN, 56039")
+print("Caleb Jostad")  
+print("2857 70th st, Granada MN, 56039")  
 print("763-234-3496")
 print("Computer science")
